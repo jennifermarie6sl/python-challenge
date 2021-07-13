a1ec7d24402ff37cb8bdd7aae8761ea134173c53
jennifermarie6sl <85765364+jennifermarie6sl@users.noreply.github.com>
2021-07-13 16:24:34 -0700

    Most Calcs working

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,36 +8,73 @@ analysis_file = os.path.join("PyBank", "Analysis", "Analysis.txt")
 #Month Variables
 months = 0
 months_changed = []
-change_list = []
+total_months = []
      
 #Dollar Variables
-#profit_loss_values = int(budget_data[1])
-#months_total = sum(months)
+p_l_start= 0
+net_change_list = []
+increase = []
+decrease = []
 
 #CSV file management
 with open(budget_data_csv) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter= ",")
+    csv_reader = csv.reader(csv_file)
     
-    #Read the header
+    #Read/skip the header
     csv_header = next(csv_file)
-    print (f'Header: {csv_header}')
+    #print (f'Header: {csv_header}')
+    
+    #extract first row to avoid appending to total
+    first_row = next(csv_reader) 
+    #print(first_row) #tester to see if it returns value in first row
+    months = months + 1 #takes starting months value of 0 and adds 1
+    #print(months)
+    p_l_start = p_l_start + int(first_row[1])
+    #print(p_l_start)
+    net_change= int(first_row[1])
+    #print(net_change)
+    
+    for row in csv_reader:
         
-#    print sum(profit_loss)
-    
-profit_loss = []
-#for data in csv_reader:
-#            profit_loss.append(float(data[2])
-            
-#        data_dictionary = {}
-#        data_dictionary["dollars"] = profit_loss
-#        data_dictionary["sum"] = sum(data_dictionary["dollars"])
-#        print(data_dictionary)
+        #Start tracking
+        months = months + 1
+        #print(months)#prints list of count of months
+        p_l_start = p_l_start + int(first_row[1])
+        #print(p_l_start)
+        
+        #Track new changes
+        new_name= int(row[1]) - net_change
+        net_change = int(row[1])
+        net_change_list = net_change_list + [new_name]
+        months_changed = months_changed + [row[0]]
+        #print(months_changed)
+        #print(net_change_list)
+        
 
-       
-    
-    
-    
-                              
-#    print(for fiber in data_dictionary["fiber"]:
-#            print (fiber)
-#          print(sum(data_dictionary["fiber"])
+        increase = max(net_change_list)
+        #print(increase)
+        
+        decrease = min(net_change_list)
+        if row == decrease:
+            decrease[0] = row[0]
+            decrease[1] = net_change_list
+        #print(net_change)
+
+    #Calcs:
+    average = sum(net_change_list)/ len(net_change_list)
+    #print(round(average, 2))
+    #print(p_l_start)
+    print(p_l_start)
+
+        
+    output = (f"\nFinancial Analysis\n"
+        f"------------------------------------------------\n"
+        f"Total Months: {months}\n"
+        #    f"Total: ${}\n"
+        f"Average Change ${round(average, 2)}\n"
+        f"Greatest Increase in Profits: (${increase})\n"
+        f"Greastest Decrease in Profits: (${decrease})\n")
+
+    print(output)
+    #Total not working
+    #bring date and $ for increase and decrease
